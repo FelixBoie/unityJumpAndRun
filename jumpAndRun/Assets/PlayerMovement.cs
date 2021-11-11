d3cd294f0f30733ee30f8d4d62069a2877678027
FelixBoie <info@felix-boie.de>
2021-11-11 21:02:02 +0100

    Add movement via basic key inputs

--- jumpAndRun/Assets/PlayerMovement.cs
+++ jumpAndRun/Assets/PlayerMovement.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-
+    public float forwardForce = 500f;
+    public float sidewayForce = 500f;
+    public float jumpForce = 2000f;
     public Rigidbody rb; 
 
 
@@ -17,6 +19,30 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()   // unity prefers FixedUpate with forces
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime); //Time since the last time a frame was drawn
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0,0, -2.5f * forwardForce * Time.deltaTime);
+        }
+        else
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime); //add a forward force
+        }
+
+        
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0);
+        }
+        
+        if (Input.GetKey("space"))
+        {
+            rb.AddForce(0, jumpForce * Time.deltaTime, 0);
+        }
+
     }
 }
